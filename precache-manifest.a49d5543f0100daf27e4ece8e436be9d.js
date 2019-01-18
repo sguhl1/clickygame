a748ed0552a980f5d9d3e1c2726bc751e6c7419a
sguhl1 <sguhl1@yahoo.com>
2019-01-17 21:43:56 -0500

    Updates

---
+++ precache-manifest.a49d5543f0100daf27e4ece8e436be9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "54a1a6891d3feaaff433",
+    "url": "/clickygame/static/js/main.54a1a689.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/clickygame/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "54a1a6891d3feaaff433",
+    "url": "/clickygame/static/css/main.a52ff27b.chunk.css"
+  },
+  {
+    "revision": "2ef52c470b560bd2fb45a87acd95db83",
+    "url": "/clickygame/index.html"
+  }
+];
